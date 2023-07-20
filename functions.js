5ed3c5d73223c8f3f9497f329a2cf1d76357984d
git-leon <xleonhunter@gmail.com>
2023-07-19 22:04:33 -0400

    the first commit message

---
+++ functions.js
@@ -0,0 +1,14 @@
+function multiply(x, y) {
+    const product = x * y;
+    return product;
+}
+
+
+const firstProduct = multiply(10,5);
+const secondProduct = multiply(11,6);
+const thirdProduct = multiply("hello", "world");
+
+
+console.log(firstProduct);
+console.log(secondProduct);
+console.log(thirdProduct);
